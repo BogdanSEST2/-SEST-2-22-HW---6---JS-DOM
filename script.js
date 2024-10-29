e268aae9fe3e4c011b886714166f1d37b8131ded
BogdanTsybin2005 <tsybinb@gmail.com>
2024-10-29 15:38:21 +0600

    Полностью доделал самую первую страницу с ссылками на другие задания и доделал основной её функционал

--- script.js
+++ script.js
@@ -3,12 +3,30 @@ window.addEventListener('DOMContentLoaded', () => {
     const header = document.querySelector('.header');
     const footer = document.querySelector('.footer');
     const mainList = document.querySelector('.main__list');
+    const headerBurger = document.querySelector('.header__burger');
+    const headerMenu = document.querySelector('.header__menu');
+
 
     changeThemeButton.addEventListener('click', () => {
-        document.body.classList.toggle('light');
         document.documentElement.classList.toggle('light');
+        document.body.classList.toggle('light');
         header.classList.toggle('light');
         footer.classList.toggle('light');
         mainList.classList.toggle('light');
-    }); 
+    });
+
+    headerBurger.addEventListener('click', () => {
+        headerMenu.classList.toggle('__active');
+        headerBurger.classList.toggle('__active');
+        document.body.style.cssText = 'overflow: hidden;';
+    });
+
+    window.addEventListener('resize', () => {
+        if (window.innerWidth >= 768) {
+            if (headerBurger.classList.contains('__active') && headerMenu.classList.contains('__active')) {
+                headerBurger.classList.remove('__active');
+                headerMenu.classList.remove('__active');
+            }
+        }
+    });
 });
